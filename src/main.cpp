c24588ce28241ef02258f8bc930bb9ec8e5cd427
icoappdeveloper <icoappdeveloper@gmail.com>
2021-12-04 04:02:41 +0100

    Relelase 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,155 @@
-//#define USE_SERIAL
-#define USE_DISPLAY
-//#define USE_WIFI
+#define USE_SERIAL      OFF
+#define USE_DISPLAY     ON
+#define USE_WIFI        ON
+#define USE_OTA         ON
+#define USE_MQTT        ON
+#define USE_SSL         ON
 
 #include <main.h>
 #include <passwords.h>
 
 Adafruit_SCD30  scd30;
-#ifdef USE_DISPLAY
-Adafruit_SSD1306 display;
+#if USE_DISPLAY
+Adafruit_SSD1306 oled;
 #endif
-
-uint16_t refreshTime;
+#if USE_WIFI && USE_MQTT
+#if USE_SSL
+WiFiClientSecure client;
+static const char *fingerprint PROGMEM = "59 3C 48 0A B1 8B 39 4E 0D 58 50 47 9A 13 55 60 CC A0 1D AF";
+#else
+WiFiClient client;
+#endif
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish co2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/CO2");
+#endif
 
 
 void setup(void)
 {
     Wire.begin(4,5);
-#ifdef USE_WIFI
-    WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
-#endif
-#ifdef USE_SERIAL
+
+#if USE_SERIAL
     Serial.begin(115200);
     while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 #endif
-#ifdef USE_DISPLAY
-    display = Adafruit_SSD1306(128, 64, &Wire);
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+
+#if USE_DISPLAY
+    oled = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+    oled.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+    oled.setTextColor(SSD1306_WHITE);
+    oled.cp437(true);
+#endif
+
+    _printf("WiFi: %s\n",USE_WIFI ? TEXT_ON : TEXT_OFF);
+    cursor(getCursorX(), getCursorY() + 1);
+    _printf("OTA: %s\n",USE_OTA ? TEXT_ON : TEXT_OFF);
+    cursor(getCursorX(), getCursorY() + 1);
+    _printf("MQTT: %s\n",USE_MQTT ? TEXT_ON : TEXT_OFF);
+    cursor(getCursorX(), getCursorY() + 1);
+    _printf("SSL: %s\n",USE_SSL ? TEXT_ON : TEXT_OFF);
+
+    _show();
+    _clear();
+
+#if USE_WIFI
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
+    uint8 attempts;
+    for(attempts = 0; attempts < CONNECTION_RETRY_COUNT; attempts++)
+    {
+        delay(CONNECTION_RETRY_INTERVAL);
+        if(WiFi.isConnected())
+            break;
+        else
+        {
+            WiFi.reconnect();
+            if(attempts == 0)
+                _printf("Connecting to %s\n",WLAN_SSID);
+            _printf("Attempt %i of %i...\n",attempts + 1,CONNECTION_RETRY_COUNT);
+            _show();
+        }
+    }
+    if(!WiFi.isConnected())
+    {
+        _print("Connection failed.\nRebooting...");
+        _show();
+        delay(500);
+        ESP.restart();
+    }
+
+#if USE_OTA
+    ArduinoOTA.onStart([]()
+                       {
+                           _clear();
+                           _println("OTA update utility\n");
+                           _show();
+                       });
+    ArduinoOTA.onEnd([]()
+                     {
+                         _print("\n\nOTA update ended");
+                         _show();
+                     });
+#if USE_DISPLAY
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+    {
+        oled.fillRect(getCursorX(), getCursorY(), progress * SCREEN_WIDTH / total, 10, SSD1306_WHITE);
+        _show();
+    });
+#endif
+    ArduinoOTA.onError([](ota_error_t error) {
+        _printf("\n\nError %u: ",error);
+        switch (error)
+        {
+            case OTA_AUTH_ERROR:
+            {
+                _println("auth failed");
+                break;
+            }
+            case OTA_BEGIN_ERROR:
+            {
+                _println("begin failed");
+                break;
+            }
+            case OTA_CONNECT_ERROR:
+            {
+                _println("connect failed");
+                break;
+            }
+            case OTA_RECEIVE_ERROR:
+            {
+                _println("receive failed");
+                break;
+            }
+            case OTA_END_ERROR:
+            {
+                _println("end failed");
+                break;
+            }
+            default:
+            {
+                _println("unknown");
+            }
+        }
+        _show();
+    });
+    ArduinoOTA.begin();
+#endif
+#ifdef USE_SSL
+    client.setFingerprint(fingerprint);
+#endif
 #endif
 
     ledBuiltinSetup();
     buzzer(BUZZER_OFF);
     buzzerSetup();
 
-    show();
-    delay(1000);
-    clear();
-    display.setTextColor(SSD1306_WHITE);
-    display.cp437(true);
-
     //Serial.println("Adafruit SCD30 test!");
 
     // Try to initialize!
     if (!scd30.begin())
     {
-        println("Failed to find SCD30 chip.");
-        show();
+        _println("Failed to find SCD30 chip.");
+        _show();
         //Serial.println("Failed to find SCD30 chip");
         while (1) { delay(10); }
     }
@@ -55,7 +160,6 @@ void setup(void)
     //   Serial.println("Failed to set measurement interval");
     //   while(1){ delay(10);}
     // }
-    refreshTime = scd30.getMeasurementInterval() * 1000;
 }
 
 int16_t x,y;
@@ -63,23 +167,44 @@ bool first = true;
 bool alarm = false;
 
 void loop() {
+#if USE_WIFI && USE_OTA
+    ArduinoOTA.handle();
+#endif
     if (scd30.dataReady())
     {
         if (!scd30.read())
         {
-            println("Error reading sensor data.");
+            _println("Error reading sensor data.");
             return;
         }
         ledBuiltin(LED_ON);
-        clear();
-        printf("%s\n\n",SENSOR_NAME);
+        _clear();
+
+#if USE_WIFI && USE_MQTT
+        if(!mqtt.connected())
+        {
+            _println("MQTT not connected,\nreconnecting...:\n");
+            _show();
+            int8_t result = mqtt.connect();
+            if(result)
+            {
+                _printf("Not connected:\n%s",(char*)mqtt.connectErrorString(result));
+            }
+            else
+                _print("Connected!");
+            _show();
+            delay(500);
+            _clear();
+        }
+#endif
+        _printf("%s\n\n",SENSOR_NAME);
         if(scd30.temperature != 0)
         {
-            printf("Temperature: %.1f%c C\n",scd30.temperature,CHAR_DEGREE);
-            display.setCursor(display.getCursorX(),display.getCursorY() + 3);
+            _printf("Temperature: %.1f%c C\n",scd30.temperature,CHAR_DEGREE);
+            cursor(getCursorX(), getCursorY() + 3);
         }
         if(scd30.relative_humidity != 0)
-            printf("Humidity: %.1f %%\n\n",scd30.relative_humidity);
+            _printf("Humidity: %.1f %%\n\n",scd30.relative_humidity);
 
         if(first)
         {
@@ -87,32 +212,34 @@ void loop() {
         }
         else if(scd30.CO2 != 0)
         {
-            setSize(2);
-            print("CO");
-            x = display.getCursorX();
-            y = display.getCursorY();
-            setSize(1);
-            print("\n");
-            display.setCursor(x,display.getCursorY());
-            print("2 ");
-            setSize(2);
-            display.setCursor(display.getCursorX(),y);
-            printf("%.0f",scd30.CO2);
-            x = display.getCursorX();
-            setSize(1);
+            textSize(2);
+            _print("CO");
+            x = getCursorX();
+            y = getCursorY();
+            textSize(1);
+            _print("\n");
+            cursor(x, getCursorY());
+            _print("2 ");
+            textSize(2);
+            cursor(getCursorX(), y);
+            _printf("%.0f",scd30.CO2);
+            x = getCursorX();
+            textSize(1);
             alarm = scd30.CO2 > SENSOR_CO2_LEVEL_DANGER;
             if(alarm)
             {
-                println(" ALARM!");
+                _println(" ALARM!");
                 buzzer(BUZZER_ON);
             }
             else
-                print("\n");
-            display.setCursor(x,display.getCursorY());
-            //display.setCursor(display.getCursorX(),display.getCursorY() + 1);
-            print(" ppm");
+                _print("\n");
+            cursor(x, getCursorY());
+            //display.cursor(display.getCursorX(),display.getCursorY() + 1);
+            _print(" ppm");
+
+            co2.publish(scd30.CO2,0);
         }
-        show();
+        _show();
         ledBuiltin(LED_OFF);
     }
 
@@ -120,8 +247,7 @@ void loop() {
     {
         delay(SENSOR_ALARM_DURATION);
         buzzer(BUZZER_OFF);
-        delay(refreshTime - SENSOR_ALARM_DURATION);
     }
     else
-        delay(refreshTime);
+        delay(100);
 }
--- src/main.h
+++ src/main.h
@@ -3,23 +3,97 @@
 //
 
 #ifndef SCD30_MAIN_H
-#define SCD0_MAIN_H
+#define SCD30_MAIN_H
+
+#define ON                  1
+#define OFF                 0
 
 #include <Arduino.h>
-
-#ifdef USE_WIFI
-#ifdef ESP8266
-#include <ESP8266WiFi.h>
-#else //ESP32
-#include <WiFi.h>
-#endif
-#include <Adafruit_MQTT.h>
-#include <Adafruit_MQTT_Client.h>
-#endif
 #include <Adafruit_SCD30.h>
 #include <Wire.h>
-#ifdef USE_DISPLAY
-#include <Adafruit_SSD1306.h>
+
+#if USE_WIFI
+    #define CONNECTION_RETRY_COUNT      5
+    #define CONNECTION_RETRY_INTERVAL   5000
+
+    #if ESP8266
+        #include <ESP8266WiFi.h>
+    #else
+        #include <WiFi.h>
+    #endif
+
+    #if USE_OTA
+        #include <ArduinoOTA.h>
+    #endif
+
+    #if USE_MQTT
+        #include <Adafruit_MQTT.h>
+        #include <Adafruit_MQTT_Client.h>
+    #endif
+
+#endif
+
+#if USE_SERIAL
+    #define printSerial(x)          Serial.print(x)
+    #define printfSerial(x,y...)    Serial.printf(x,y)
+    #define printlnSerial(x)        Serial.println(x)
+#endif
+
+#if USE_DISPLAY
+    #include <Adafruit_SSD1306.h>
+    #define SCREEN_WIDTH            128
+    #define SCREEN_HEIGHT           64
+
+    #define printDisplay(x)                 oled.print(x)
+    #define printfDisplay(format, args...)  oled.printf(format, args)
+    #define printlnDisplay(x)               oled.println(x)
+    #define showDisplay()                   oled.display()
+    #define setDisplaySize(x)               oled.setTextSize(x)
+    #define getDisplayCursorX()             oled.getCursorX()
+    #define getDisplayCursorY()             oled.getCursorY()
+    #define setDisplayCursor(x,y)           oled.setCursor(x,y)
+    #define setDisplayCursorX(x)            oled.setCursor(x,getDisplayCursorY())
+    #define setDisplayCursorY(y)            oled.setCursor(getDisplayCursorX(),y)
+    #define clearDisplay()                  oled.clearDisplay(); setDisplayCursor(0,0)
+
+#endif
+
+#if USE_SERIAL && USE_DISPLAY
+    #define _print(x)               printDisplay(x); printSerial(x)
+    #define _printf(x,y...)         printfDisplay(x,y); printfSerial(x,y)
+    #define _println(x)             printlnDisplay(x); printlnSerial(x)
+    #define _clear()                clearDisplay()
+    #define _show()                 showDisplay()
+    #define textSize(x)             setDisplaySize(x)
+    #define getCursorX()            getDisplayCursorX()
+    #define getCursorY()            getDisplayCursorY()
+    #define cursor(x,y)             setDisplayCursor(x,y)
+    #define setCursorX(x)           setDisplayCursorX(x)
+    #define setCursorY(y)           setDisplayCursorY(y)
+#elif USE_SERIAL
+    #define _print(x)               printSerial(x)
+    #define _printf(x,y...)         printfSerial(x,y)
+    #define _println(x)             printlnSerial(x)
+    #define _clear()
+    #define _show()
+    #define textSize(x)
+    #define getCursorX()
+    #define getCursorY()
+    #define cursor(x,y)
+    #define setCursorX(x)
+    #define setCursorY(y)
+#elif USE_DISPLAY
+    #define _print(x)               printDisplay(x)
+    #define _printf(x,y...)         printfDisplay(x,y)
+    #define _println(x)             printlnDisplay(x)
+    #define _clear()                clearDisplay()
+    #define _show()                 showDisplay()
+    #define textSize(x)             setDisplaySize(x)
+    #define getCursorX()            getDisplayCursorX()
+    #define getCursorY()            getDisplayCursorY()
+    #define cursorX(x)              setDisplayCursorX(x)
+    #define cursorY(y)              setDisplayCursorY(y)
+    #define cursor(x,y)             setDisplayCursor(x,y)
 #endif
 
 #define SENSOR_NAME                 "Sensirion SCD30"
@@ -27,6 +101,8 @@
 #define SENSOR_ALARM_DURATION       200
 
 #define CHAR_DEGREE         (char)248
+#define TEXT_ON             "ON"
+#define TEXT_OFF            "OFF"
 
 #define BUZZER              LED_BUILTIN_AUX
 
@@ -42,43 +118,5 @@
 #define out(x,y)            digitalWrite(x,y)
 #define ledBuiltin(x)       out(LED_BUILTIN,x)
 #define buzzer(x)           out(BUZZER,x)
-
-#ifdef USE_SERIAL
-#define printSerial(x)          Serial.print(x)
-#define printfSerial(x,y...)    Serial.printf(x,y)
-#define printlnSerial(x)        Serial.println(x)
-#endif
-
-#ifdef USE_DISPLAY
-#define printDisplay(x)         display.print(x)
-#define printfDisplay(x,y...)   display.printf(x,y)
-#define printlnDisplay(x)       display.println(x)
-#define clearDisplay()          display.clearDisplay(); display.setCursor(0,0)
-#define showDisplay()           display.display()
-#define setDisplaySize(x)       display.setTextSize(x)
-#endif
-
-#if defined(USE_SERIAL) && defined(USE_DISPLAY)
-#define print(x)                printDisplay(x); printSerial(x)
-#define printf(x,y...)          printfDisplay(x,y); printfSerial(x,y)
-#define println(x)              printlnDisplay(x); printlnSerial(x)
-#define clear()                 clearDisplay()
-#define show()                  showDisplay()
-#define setSize(x)              setDisplaySize(x)
-#elif defined(USE_SERIAL)
-#define print(x)                printSerial(x)
-#define printf(x,y...)          printfSerial(x,y)
-#define println(x)              printlnSerial(x)
-#define clear()
-#define show()
-#define setSize(x)
-#elif defined(USE_DISPLAY)
-#define print(x)                printDisplay(x)
-#define printf(x,y...)          printfDisplay(x,y)
-#define println(x)              printlnDisplay(x)
-#define clear()                 clearDisplay()
-#define show()                  showDisplay()
-#define setSize(x)              setDisplaySize(x)
-#endif
 
 #endif //SCD30_MAIN_H
