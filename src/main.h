1b47f117fa6be9ab5a7927fb2fcb0370d021f4b3
ico2k2developer <icoappdeveloper@gmail.com>
2025-06-05 12:24:18 +0200

    Synced with currently running code

--- src/main.h
+++ src/main.h
@@ -52,6 +52,26 @@
     #define SCREEN_WIDTH            128
     #define SCREEN_HEIGHT           64
 
+    #if USE_DISPLAY_DELAY
+        #define REFRESH_DISPLAY 3000
+        #define REFRESH_RESTART 5000
+
+        #define setupDisplayDelay()     unsigned long __display_delay = 0
+
+        
+        #define showDisplay()          do{unsigned long ms = millis(); \
+            if(ms - __display_delay < REFRESH_DISPLAY) oled.display(); \
+            else if(ms - __display_delay < REFRESH_RESTART) {clearDisplay(); oled.display(); }\
+            else __display_delay = ms; }while(0)
+
+        #define forceShowDisplay()      oled.display(); __display_delay = millis();
+        
+    #else
+        #define showDisplay()           oled.display()
+        #define forceShowDisplay()      showDisplay()
+    #endif
+
+
     #define printDisplay(x)             oled.print(x)
     #define printfDisplay(x,y...)       oled.printf(x,y)
     #define printlnDisplay(x)           oled.println(x)
@@ -59,7 +79,6 @@
     #define rtlprintDisplay(x)          rtlprint(x)
     #define rtlprintfDisplay(x,y...)    rtlprintf(x,y)
     #define rtlprintlnDisplay(x)        rtlprintln(x)
-    #define showDisplay()               oled.display()
     #define setDisplaySize(x)           oled.setTextSize(x)
     #define getDisplayCursorX()         oled.getCursorX()
     #define getDisplayCursorY()         oled.getCursorY()
@@ -76,6 +95,7 @@
     #define _println(x)                 printlnDisplay(x); printlnSerial(x)
     #define _clear()                    clearDisplay()
     #define _show()                     showDisplay()
+    #define _forceShow()                forceShowDisplay()
     #define _textSize(x)                setDisplaySize(x)
     #define _cursorX()                  getDisplayCursorX()
     #define _cursorY()                  getDisplayCursorY()
@@ -91,6 +111,7 @@
     #define _println(x)                 printlnSerial(x)
     #define _clear()
     #define _show()
+    #define _forceShow()
     #define _textSize(x)
     #define _cursorX()
     #define _cursorY()
@@ -101,20 +122,21 @@
     #define _rtlprintf(x,y...)          printfSerial(x,y)
     #define _rtlprintln(x)              printlnSerial(x)
 #elif USE_DISPLAY
-    #define _print(x)               printDisplay(x)
-    #define _printf(x,y...)         printfDisplay(x,y)
-    #define _println(x)             printlnDisplay(x)
-    #define _clear()                clearDisplay()
-    #define _show()                 showDisplay()
-    #define _textSize(x)            setDisplaySize(x)
-    #define _cursorX()              getDisplayCursorX()
-    #define _cursorY()              getDisplayCursorY()
-    #define _setCursor(x,y)         setDisplayCursor(x,y)
-    #define _setCursorX(x)          setDisplayCursorX(x)
-    #define _setCursorY(y)          setDisplayCursorY(y)
-    #define _rtlprint(x)            rtlprintDisplay(x)
-    #define _rtlprintf(x,y...)      rtlprintfDisplay(x,y)
-    #define _rtlprintln(x)          rtlprintlnDisplay(x)
+    #define _print(x)                   printDisplay(x)
+    #define _printf(x,y...)             printfDisplay(x,y)
+    #define _println(x)                 printlnDisplay(x)
+    #define _clear()                    clearDisplay()
+    #define _show()                     showDisplay()
+    #define _forceShow()                forceShowDisplay()
+    #define _textSize(x)                setDisplaySize(x)
+    #define _cursorX()                  getDisplayCursorX()
+    #define _cursorY()                  getDisplayCursorY()
+    #define _setCursor(x,y)             setDisplayCursor(x,y)
+    #define _setCursorX(x)              setDisplayCursorX(x)
+    #define _setCursorY(y)              setDisplayCursorY(y)
+    #define _rtlprint(x)                rtlprintDisplay(x)
+    #define _rtlprintf(x,y...)          rtlprintfDisplay(x,y)
+    #define _rtlprintln(x)              rtlprintlnDisplay(x)
 #endif
 
 #define SENSOR_NAME                 "Sensirion SCD30"
@@ -124,7 +146,7 @@
 #define CHAR_DEGREE         (char)248
 #define TEXT_ON             "ON"
 #define TEXT_OFF            "OFF"
-#define VERSION             "3.1"
+#define VERSION             "3.3"
 #define VERSION_NAME        "v" VERSION
 
 #define BUZZER              LED_BUILTIN_AUX
@@ -152,21 +174,22 @@
     #define WIFI_SSID_MAX       32
     #define WIFI_PASSWORD_MAX   64
 
-    const char WIFI00_SSID[] PROGMEM =      WIFI0_SSID;
-    const char WIFI01_SSID[] PROGMEM =      WIFI1_SSID;
-    const char WIFI02_SSID[] PROGMEM =      WIFI2_SSID;
+    const char MEM_DEFAULT_SSID[] PROGMEM =     DEFAULT_SSID;
+    const char MEM_WIFI01_SSID[] PROGMEM =      WIFI01_SSID;
+    const char MEM_WIFI02_SSID[] PROGMEM =      WIFI02_SSID;
 
-    const char WIFI00_PASSWORD[] PROGMEM =   WIFI0_PASSWORD;
-    const char WIFI01_PASSWORD[] PROGMEM =   WIFI1_PASSWORD;
-    const char WIFI02_PASSWORD[] PROGMEM =   WIFI2_PASSWORD;
+    const char MEM_DEFAULT_PASSWORD[] PROGMEM = DEFAULT_PASSWORD;
+    const char MEM_WIFI01_PASSWORD[] PROGMEM =  WIFI01_PASSWORD;
+    const char MEM_WIFI02_PASSWORD[] PROGMEM =  WIFI02_PASSWORD;
 
-    #define WIFI_COUNT                              3
-    const char* const WIFI_SSIDS[] PROGMEM =        {WIFI00_SSID,WIFI01_SSID,WIFI02_SSID};
-    const char* const WIFI_PASSWORDS[] PROGMEM =    {WIFI00_PASSWORD,WIFI01_PASSWORD,WIFI02_PASSWORD};
+    const char* const WIFI_SSIDS[] PROGMEM =        {MEM_DEFAULT_SSID,MEM_WIFI01_SSID,MEM_WIFI02_SSID};
+    const char* const WIFI_PASSWORDS[] PROGMEM =    {MEM_DEFAULT_PASSWORD,MEM_WIFI01_PASSWORD,MEM_WIFI02_PASSWORD};
+
+    #define WIFI_COUNT  3
 
     #if USE_MQTT
 
-        #define AIO_SERVER      "io.adafruit.com"
+        #define AIO_SERVER      "homeassistant.local"
 
         #if USE_SSL
             #define AIO_SERVERPORT  8883
